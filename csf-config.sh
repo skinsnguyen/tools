84b5854583b5fce3a446f8d23fee1159bed07068
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2024-01-20 15:22:12 +0700

    Update csf-config.sh

--- csf-config.sh
+++ csf-config.sh
@@ -1,3 +1,18 @@
+sed -i 's/^TESTING = .*/TESTING = "0"/g' /etc/csf/csf.conf
+sed -i 's/^RESTRICT_SYSLOG = .*/RESTRICT_SYSLOG = "3"/g' /etc/csf/csf.conf
+sed -i 's/^TESTING_INTERVAL = .*/TESTING_INTERVAL = "5"/g' /etc/csf/csf.conf
+sed -i 's/^LF_CSF = .*/LF_CSF =  = "1"/g' /etc/csf/csf.conf
+sed -i 's/^UDPFLOOD = .*/UDPFLOOD = "1"/g' /etc/csf/csf.conf
+sed -i 's/^UDPFLOOD_LIMIT = .*/UDPFLOOD_LIMIT = "25/s"/g' /etc/csf/csf.conf
+sed -i 's/^UDPFLOOD_BURST = .*/UDPFLOOD_BURST = "50"/g' /etc/csf/csf.conf
+sed -i 's/^CT_LIMIT = .*/CT_LIMIT = "50"/g' /etc/csf/csf.conf
+
+
+
+
+
+
+#############################################################################################
 sed -i '/^#SPAMDROP/s/^#//' /etc/csf/csf.blocklists
 sed -i '/^#SPAMEDROP/s/^#//' /etc/csf/csf.blocklists
 sed -i '/^#DSHIELD/s/^#//' /etc/csf/csf.blocklists
@@ -27,4 +42,16 @@ cat > /etc/csf/csf.rignore << EOF
 .search.msn.com
 EOF
 #############################################################################################
+sed -i '/gmail.com/d' /etc/csf/csf.dyndns
+sed -i '/public.pyzor.org/d' /etc/csf/csf.dyndns
+echo "tcp|out|d=25|d=smtp.gmail.com" >> /etc/csf/csf.dyndns
+echo "tcp|out|d=465|d=smtp.gmail.com" >> /etc/csf/csf.dyndns
+echo "tcp|out|d=587|d=smtp.gmail.com" >> /etc/csf/csf.dyndns
+echo "tcp|out|d=995|d=imap.gmail.com" >> /etc/csf/csf.dyndns
+echo "tcp|out|d=993|d=imap.gmail.com" >> /etc/csf/csf.dyndns
+echo "tcp|out|d=143|d=imap.gmail.com" >> /etc/csf/csf.dyndns
+echo "udp|out|d=24441|d=public.pyzor.org" >> /etc/csf/csf.dyndns
+#############################################################################################
+
+
 
