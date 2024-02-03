efdd56690ee03551320f4c414f214d7ee433e59f
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2024-02-03 17:44:07 +0700

    Update menu-plugin.sh

--- menu-plugin.sh
+++ menu-plugin.sh
@@ -15,8 +15,8 @@ install_and_activate_plugin() {
 }
 # Hàm cài đặt và kích hoạt Theme
 install_and_activate_theme() {
-    plugin_url=$1
-    wp theme install "$plugin_url" --activate --allow-root
+    theme_url=$1
+    wp theme install "$theme_url" --activate --allow-root
 }
 # Hàm hiển thị danh sách plugin đã cài đặt
 display_installed_plugins() {
@@ -69,6 +69,32 @@ wp plugin update --all --allow-root
 show_theme_list() {
 wp theme list --allow-root
 }
+##-Danh Sách Plugin
+rocket="https://tool.kienthuclinux.info/plugin/wp-rocket_3.12.5.3.zip"
+iThemesSecurityPro="https://tool.kienthuclinux.info/plugin/ithemes-security-pro-8.0.2.zip"
+WPMUDevDashboard="https://tool.kienthuclinux.info/plugin/wpmu-dev-dashboard.zip"
+SEORankMathPro="https://tool.kienthuclinux.info/plugin/seo-by-rank-math-pro.zip"
+Elementskit="https://tool.kienthuclinux.info/plugin/elementskit-3.2.1.zip"
+sitepress-multilingual="https://tool.kienthuclinux.info/plugin/sitepress-multilingual-cms.4.5.5.zip"
+seedprod="https://tool.kienthuclinux.info/plugin/seedprod.zip"
+wp-staging="https://tool.kienthuclinux.info/plugin/wp-staging-pro.zip"
+migration-unlimited-extension="https://tool.kienthuclinux.info/plugin/all-in-one-wp-migration-unlimited-extension.zip"
+
+
+#Theme-Astra
+astra-child="https://tool.kienthuclinux.info/theme/astra-child.zip"
+astra="https://tool.kienthuclinux.info/theme/astra.4.0.2.zip"
+astra-addon-plugin="https://tool.kienthuclinux.info/theme/astra-addon-plugin-4.0.1.zip"
+astra-premium-sites="https://tool.kienthuclinux.info/theme/astra-premium-sites-3.1.24.zip"
+all-in-one-wp-migration="https://tool.kienthuclinux.info/plugin/all-in-one-wp-migration.zip"
+wp-schema-pro="https://tool.kienthuclinux.info/theme/wp-schema-pro-2.7.4.zip"
+ultimate-elementor="https://tool.kienthuclinux.info/theme/ultimate-elementor-1.36.14.zip"
+convertpro-addon="https://tool.kienthuclinux.info/theme/convertpro-addon-1.5.5.zip"
+bb-ultimate-addon="https://tool.kienthuclinux.info/theme/bb-ultimate-addon-1.35.3.zip"
+
+
+
+
 
 
 # Menu Script
