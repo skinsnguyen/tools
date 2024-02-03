caaadc5e53b0756578ee00dbede184d56cdf3513
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2024-02-03 16:59:45 +0700

    Update menu-plugin.sh

--- menu-plugin.sh
+++ menu-plugin.sh
@@ -71,9 +71,92 @@ wp theme list --allow-root
 }
 
 
+# Menu Script
+install_plugin() {
+    echo "+---------------------------------------------+"
+    echo "| Chọn Script mà bạn muốn cài:                |"
+    echo "+---------------------------------------------+"
+    echo "| 1.                 |"
+    echo "| 2.                          |"
+    echo "| 3.                           |"
+    echo "| 4.                     |"
+    echo "| 5.                    |"
+    echo "| 6.                          |"
+    echo "| 7.                   |"
+    echo "| 8.                           |"
+    echo "| 9.                            |"
+    echo "| 10.                      |"
+    echo "| 0. Quay lại Menu Chính                      |"
+    echo "+---------------------------------------------+"
+    read -p "Nhập vào lựa chọn: " choice_plugin
+    case $choice_plugin in
+        0) install_and_activate_plugin ;;
+        1) install_hostvn_script ;;
+        2) install_hocvps ;;
+        3) install_larvps ;;
+        4) install_centmind_mod ;;
+        5) install_tinovps_script ;;
+        6) install_webinoly_script ;;
+        7) install_easyengine_script ;;
+        8) install_wordops_script ;;
+        9) install_dlemp_script ;;
+        10) install_lempstack ;;
+        *) echo "Lựa chọn không hợp lệ. Thoát." && exit 1 ;;
+    esac
+}
+
+install_themes(){
+    echo "+---------------------------------------------+"
+    echo "| Chọn Control Panel mà bạn muốn cài:         |"
+    echo "+---------------------------------------------+"
+    echo "| 1. Cài đặt HestiaCP                         |"
+    echo "| 2. Cài đặt CloudPanel                       |"
+    echo "| 3. Cài đặt AApanel                          |"
+    echo "| 4. Cài đặt FastPanel                        |"
+    echo "| 5. Cài đặt CyberPanel                       |"
+    echo "| 6. Cài đặt CWP (Control-WebPanel)           |"
+    echo "| 7. Cài đặt Webmin                           |"
+    echo "| 8. Cài đặt DirectAdmin                      |"
+    echo "| 9. Cài đặt VestaCP                          |"
+    echo "| 0. Quay lại Menu Chính                      |"
+    echo "+---------------------------------------------+"
+    read -p "Nhập vào lựa chọn: " control_panel_choice
+    case $control_panel_choice in
+        0) back_to_main_menu ;;
+        1) install_hestiacp ;;
+        2) install_cloudpanel ;;
+        3) install_aapanel ;;
+        4) install_fastpanel ;;
+        5) install_cyberpanel ;;
+        6) install_cwp ;;
+        7) install_webmin ;;
+        8) install_directadmin ;;
+        9) install_vestacp ;;
+        *) echo "Lựa chọn không hợp lệ. Thoát." && exit 1 ;;
+    esac
+}
 
 
+#main_menu
+main_menu() {
+    # Các phần hiển thị thông tin máy chủ và hệ điều hành ở đây
 
+    echo "+---------------------------------------------+"
+    echo "| Chọn loại cài đặt mà bạn muốn thực hiện:    |"
+    echo "+---------------------------------------------+"
+    echo "| 1. cai đặt theme                            |"
+    echo "| 2. Cài đặt plugin                           |"
+    echo "| 0. Thoát                                    |"
+    echo "+---------------------------------------------+"
+    read -p "Nhập vào lựa chọn: " main_choice
+    case $main_choice in
+        0) exit ;;
+        1) install_plugin ;;
+        2) install_themes ;;
+        *) echo "Lựa chọn không hợp lệ. Thoát." && exit 1 ;;
+    esac
+}
 
+main_menu
 
 #rm -f ${pwdd}/plugin-cli-new.sh;
