61d3c891a983d7b6215a2028aebe70c593d9ed43
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2024-08-07 10:44:51 +0700

    Create disable_functions-Cyberpanel.sh

---
+++ disable_functions-Cyberpanel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Nam-Nguyen
+# 07/08/2024
+# Tăng các thống ở configurations all tất php
+
+lsws_path="/usr/local/lsws"
+timess=$(date +"%Hh-%Mp-%F")
+if [ -d "${lsws_path}" ]; then
+    configurations="disable_functions =exec,passthru,shell_exec,system,symlink,proc_open,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source
+allow_url_fopen=Off
+allow_url_include=Off
+"
+for i in $(ls "${lsws_path}" | grep 'lsphp*'); do
+        php_d_path="${lsws_path}/${i}/etc/php.d"
+        nam_ini_file="${php_d_path}/namnh-cu.ini"
+
+        echo "Adding configurations to ${nam_ini_file}"
+
+        echo "${configurations}" >> "${nam_ini_file}"
+
+        echo "Configurations added to ${nam_ini_file}"
+    done
+else
+    echo "Thư mục ${lsws_path} không tồn tại."
+fi
