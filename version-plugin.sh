7f97fd842da89ad21b06003e64855a1cbe0860c3
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2024-10-29 13:18:29 +0700

    Create version-plugin.sh

---
+++ version-plugin.sh
@@ -0,0 +1,47 @@
+#Theme Astra
+export Astra_main="4.8.1"
+export astra_child=""
+#Plugin Astra
+export astra_addon_plugin="4.8.1"
+export astra_premium_sites="4.4.2"
+export astra_portfolio="1.11.9"
+export bb_ultimate-addon="1.35.23"
+export convertpro="1.7.7"
+export convertpro_addon="1.5.10"
+export ultimate_elementor="1.36.36"
+export wp_schema_pro="2.7.19"
+####################################################
+###########Divi Theme"##########################
+export divi_main=""
+export divi-builder=""
+########################################
+# Bộ Plugin WPDeveloper
+export essential_addons_elementor="6.0.3"
+export wp_scheduled_posts_pro="5.0.12"
+export notificationx_pro="2.9.6"
+export betterdocs_pro="3.4.5"
+export embedpress_pro="3.6.5"
+export betterlinks_pro="2.1.1"
+export essential_blocks_pro="1.9.1"
+export better_payment_pro="1.0.5"
+####################################
+export elementskit="3.6.9"
+export ithemes-security_pro="8.5.0"
+export otgs_installer="3.1.3"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
