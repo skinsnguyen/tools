b0b650535723e52194ff79fbad936fcbff17d2a2
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2024-11-15 14:10:25 +0700

    Update version-plugin.sh

--- version-plugin.sh
+++ version-plugin.sh
@@ -1,33 +1,33 @@
 #Theme Astra
-export Astra_main="4.8.1"
-export astra_child=""
+#export Astra_main="4.8.1"
+#export astra_child=""
 #Plugin Astra
-export astra_addon_plugin="4.8.1"
-export astra_premium_sites="4.4.2"
-export astra_portfolio="1.11.9"
-export bb_ultimate-addon="1.35.23"
-export convertpro="1.7.7"
-export convertpro_addon="1.5.10"
-export ultimate_elementor="1.36.36"
-export wp_schema_pro="2.7.19"
+#export astra_addon_plugin="4.8.1"
+#export astra_premium_sites="4.4.2"
+#export astra_portfolio="1.11.9"
+#export bb_ultimate-addon="1.35.23"
+#export convertpro="1.7.7"
+#export convertpro_addon="1.5.10"
+#export ultimate_elementor="1.36.36"
+#export wp_schema_pro="2.7.19"
 ####################################################
 ###########Divi Theme"##########################
-export divi_main=""
-export divi-builder=""
+#export divi_main=""
+#export divi-builder=""
 ########################################
 # Bộ Plugin WPDeveloper
-export essential_addons_elementor="6.0.3"
-export wp_scheduled_posts_pro="5.0.12"
-export notificationx_pro="2.9.6"
-export betterdocs_pro="3.4.5"
-export embedpress_pro="3.6.5"
-export betterlinks_pro="2.1.1"
-export essential_blocks_pro="1.9.1"
-export better_payment_pro="1.0.5"
+#export essential_addons_elementor="6.0.3"
+#export wp_scheduled_posts_pro="5.0.12"
+#export notificationx_pro="2.9.6"
+#export betterdocs_pro="3.4.5"
+#export embedpress_pro="3.6.5"
+#export betterlinks_pro="2.1.1"
+#export essential_blocks_pro="1.9.1"
+#export better_payment_pro="1.0.5"
 ####################################
-export elementskit="3.6.9"
-export ithemes-security_pro="8.5.0"
-export otgs_installer="3.1.3"
+#export elementskit="3.6.9"
+#export ithemes-security_pro="8.5.0"
+#export otgs_installer="3.1.3"
 
 
 ################-Theme-Astra-##################
@@ -36,16 +36,16 @@ astra="https://tainguyenwp.azdigi.com/WordPress-Astra/Theme/astra.4.8.1.zip"
 astrachild="https://tainguyenwp.azdigi.com/WordPress-Astra/Theme/astra-child.zip"
 
 ################-Plugin-Astra-##################
-astra_addon_plugin="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-addon-plugin-4.8.1.zip"
-astra_premium_sites="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-premium-sites-4.4.2.zip"
-astra_portfolio="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-portfolio-1.11.9.zip"
-bb_ultimate_addon="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/bb-ultimate-addon-1.35.23.zip"
+astraaddon_plugin="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-addon-plugin-4.8.1.zip"
+astrapremium_sites="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-premium-sites-4.4.2.zip"
+astraportfolio="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/astra-portfolio-1.11.9.zip"
+bbultimate_addon="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/bb-ultimate-addon-1.35.23.zip"
 convertpro="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/convertpro-1.7.7.zip"
 convertpro_addon="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/convertpro-addon-1.5.10.zip"
 ultimate_elementor="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/ultimate-elementor-1.36.36.zip"
-wp_schema_pro="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/wp-schema-pro-2.7.19.zip"
+wpschema_pro="https://tainguyenwp.azdigi.com/WordPress-Astra/Plugin/wp-schema-pro-2.7.19.zip"
 
-================-Theme-DiVi-================
+############################-Theme-DiVi-############################
 
 divi="https://tainguyenwp.azdigi.com/divi/Divi.zip"
 divi_builder="https://tainguyenwp.azdigi.com/divi/divi-builder.zip"
@@ -55,33 +55,100 @@ bricks="https://tainguyenwp.azdigi.com/wordpress-themes-other/Bricks.1.11.zip"
 
 ##############-Plugin-WPDeveloper-##############
 
-essential_addons_elementor="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/essential-addons-elementor.6.0.3.zip"
-wp_scheduled_posts_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/wp-scheduled-posts-pro.5.0.12.zip"
+essentialaddons_elementor="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/essential-addons-elementor.6.0.3.zip"
+wpscheduledposts_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/wp-scheduled-posts-pro.5.0.12.zip"
 notificationx_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/notificationx-pro.2.9.6.zip"
 betterdocs_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/betterdocs-pro.3.4.5.zip"
 embedpress_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/embedpress-pro.3.6.5.zip"
 betterlinks_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/betterlinks-pro.2.1.1.zip"
-essential_blocks_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/essential-blocks-pro.1.9.1.zip"
-better_payment_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/better-payment-pro.1.0.5.zip"
+essentialblocks_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/essential-blocks-pro.1.9.1.zip"
+betterpayment_pro="https://tainguyenwp.azdigi.com/wordpress-wpdeveloper/better-payment-pro.1.0.5.zip"
 
-==============-Plugin-Khac
-
-
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/elementskit-3.6.9.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/ithemes-security-pro-8.5.0.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/otgs-installer-plugin.3.1.3.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seedprod-coming-soon-pro-5-6.18.5.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seo-by-rank-math-pro.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/sitepress-multilingual-cms.4.6.13.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/swift-ai.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/wp-staging-pro.zip
-https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/speedycache-pro.zip
+############################-Plugin-Khac
 
 
-==============-Dashboard-WPMU-Dev--############################
-https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_wpmu-dev-dashboard-4.11.26.zip
-https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_smush-pro-3.16.7.zip
-https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_defender-pro-4.9.zip
+elementskit="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/elementskit-3.6.9.zip"
+ithemessecurity_pro="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/ithemes-security-pro-8.5.0.zip"
+otgsinstaller_plugin="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/otgs-installer-plugin.3.1.3.zip"
+seedprodcomingsoon_pro="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seedprod-coming-soon-pro-5-6.18.5.zip"
+seobyrankmath_pro="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/seo-by-rank-math-pro.zip"
+sitepressmultilingual_cms="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/sitepress-multilingual-cms.4.6.13.zip"
+swift_ai="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/swift-ai.zip"
+wpstaging_pro="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/wp-staging-pro.zip"
+speedycache_pro="https://tainguyenwp.azdigi.com/WordPress-Plugins-Other/speedycache-pro.zip"
+
+
+############################-Dashboard-WPMU-Dev--############################
+wpmudev_dashboard="https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_wpmu-dev-dashboard-4.11.26.zip"
+smush_pro="https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_smush-pro-3.16.7.zip"
+defender_pro="https://tainguyenwp.azdigi.com/wordpress-plugin-wpmudev/881630_defender-pro-4.9.zip"
+
+
+
+###################################################################################################################
+
+echo "################-Theme-Astra-##################"
+
+echo "$astra"
+echo "$astrachild"
+
+################-Plugin-Astra-##################
+echo "$astraaddon_plugin"
+echo "$astrapremium_sites" 
+echo "$astraportfolio"
+echo "$bbultimate_addon"
+echo "$convertpro"
+echo "$convertpro_addon"
+echo "$ultimate_elementor"
+echo "$wpschema_pro"
+
+echo "############################-Theme-DiVi-############################"
+
+echo "$divi"
+echo "$divi_builder"
+
+echo "================-Theme-Brick-================"
+echo "$bricks"
+
+##############-Plugin-WPDeveloper-##############
+
+echo "$essentialaddons_elementor"
+echo "$wpscheduledposts_pro"
+echo "$notificationx_pro"
+echo "$betterdocs_pro"
+echo "$embedpress_pro"
+echo "$betterlinks_pro"
+echo "$essentialblocks_pro"
+echo "$betterpayment_pro"
+
+echo "############################-Plugin-Khac-############################"
+
+
+echo "$elementskit"
+echo "$ithemessecurity_pro"
+echo "$otgsinstaller_plugin"
+echo "$seedprodcomingsoon_pro"
+echo "$seobyrankmath_pro"
+echo "$sitepressmultilingual_cms"
+echo "$swift_ai"
+echo "$wpstaging_pro"
+echo "$speedycache_pro"
+
+
+############################-Dashboard-WPMU-Dev--############################
+echo "$wpmudev_dashboard"
+echo "$smush_pro="
+echo "$defender_pro"
+
+
+
+
+
+
+
+
+
+
 
 
 
